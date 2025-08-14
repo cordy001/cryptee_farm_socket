e84d215824ff8cf014f6d6ad154f513a5f989b0f
cordy <mglgain@gmail.com>
2025-08-14 15:49:52 +0800

    Socket Connected

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
   },
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 app.get("/", (req, res) => {
   res.send("Socket.IO server (ESM) running");
